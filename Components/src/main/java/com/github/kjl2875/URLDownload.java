b7950fa34518aca30e58dbfb63e1589b2a95634f
PUG <kjl2875@gmail.com>
2019-04-27 20:28:29 +0900

    HTTPS DOWNLOAD MODULE

---
+++ Components/src/main/java/com/github/kjl2875/URLDownload.java
@@ -0,0 +1,30 @@
+package com.github.kjl2875;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class URLDownload
+{
+	static class Https
+	{
+		public static String get(URL url) throws IOException
+		{
+			HttpsURLConnection conn = (HttpsURLConnection)url.openConnection();
+			StringBuffer sb = new StringBuffer();
+			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String l;
+			while((l = br.readLine()) != null)
+			{
+				sb.append(l);
+				sb.append("\n");
+			}
+			conn.disconnect();
+			
+			return sb.toString();
+		}
+	}
+}
