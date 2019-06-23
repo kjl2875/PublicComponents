fd89f74451a0179121164169f00bea32b4dac17a
PUG <kjl2875@gmail.com>
2019-06-23 10:26:14 +0900

    fcopy 추가
    filecopy method

--- Components/src/main/java/com/github/kjl2875/Components/App.java
+++ Components/src/main/java/com/github/kjl2875/Components/App.java
@@ -1,6 +1,11 @@
 package com.github.kjl2875.Components;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URL;
 
 /**
@@ -19,5 +24,34 @@ public class App
     	URL url = new URL("https://example.com/");
     	File outputFile = new File("output.html");
     	URLDownload.Https.get(url,outputFile);
+    	
+    	//fcopy(new File("src"),new File("to"));
     }
+    
+    public static void fcopy(final File src, final File to) throws IOException
+	{
+		int len;
+		char []buf = new char[1024 * 1024 * 4]; // 4MB
+		
+		BufferedReader br = new BufferedReader(new FileReader(src));
+		BufferedWriter bw = new BufferedWriter(new FileWriter(to));
+	
+		try
+		{
+			while( (len = br.read(buf)) >= 0 ) {
+				bw.write(buf, 0, len);
+			}	
+		}
+		catch( IOException e )
+		{
+			//e.printStackTrace();
+			
+			bw.close();
+			br.close();
+			throw e;
+		}
+		
+		bw.close();
+		br.close();
+	}
 }
